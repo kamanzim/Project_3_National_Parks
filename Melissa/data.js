81d80604e7a70035a96e238e1e73ad4cbae85d64
kamanzim <80180381+kamanzim@users.noreply.github.com>
2021-07-23 22:23:42 -0500

    Adding test files for visualizations

---
+++ Melissa/data.js
@@ -0,0 +1,31 @@
+// Lists exported from the "Top Parks Visualizations - Transformations" jupyter notebook
+// Assigning the data to variables
+
+var top_10_park_size = {
+      acres:
+      [
+            47390,
+            76519,
+            242756,
+            801163,
+            172924,
+            32950,
+            35835,
+            337598,
+            241904,
+            46766
+      ],
+      park_names_states:
+      [
+            "Acadia National Park-ME",
+            "Arches National Park-UT",
+            "Badlands National Park-SD",
+            "Big Bend National Park-TX",
+            "Biscayne National Park-FL",
+            "Black Canyon of the Gunnison National Park-CO",
+            "Bryce Canyon National Park-UT",
+            "Canyonlands National Park-UT",
+            "Capitol Reef National Park-UT",
+            "Carlsbad Caverns National Park-NM"
+      ]
+}
