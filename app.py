cd6373d22649e5206fddb9bbe9eac247149297d5
teenastewart <teenastewart@gmail.com>
2021-07-23 20:52:13 -0500

    Updating Postgres queries to jsonify results

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 from config import password
 from config import username
-
+import psycopg2
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -13,6 +13,7 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
+username = username
 password = password
 database = "postgresql://" + username + ":" + password + "@localhost:5432/national_parks_db"
 engine = create_engine(database)
@@ -21,15 +22,14 @@ engine = create_engine(database)
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
-# print(Base.classes.keys)
-# Save reference to the table
-Parks = Base.classes.national_parks
-Visitation = Base.classes.parks_visitation
-Species = Base.classes.species
-Trails = Base.classes.park_trails
-Trailfeats = Base.classes.trail_features
-Trailacts = Base.classes.trail_activities
 
+
+
+connection = psycopg2.connect(user=username,
+                              password=password,
+                              host="localhost",
+                              port="5432",
+                              database="national_parks_db")
 #################################################
 # Flask Setup
 #################################################
@@ -55,81 +55,98 @@ def home():
 
 @app.route("/api/v1.0/Parks")
 def parks():
-# Create session 
-    session = Session(engine)
+    cursor = connection.cursor(cursor_factory = psycopg2.extras.DictCursor)
+    cursor.execute("SELECT * FROM national_parks")
+    columns = list(cursor.description)
+    result = cursor.fetchall()
+    results = []
+    for row in result:
+        row_dict = {}
+        for i, col in enumerate(columns):
+            row_dict[col.name] = row[i]
+        results.append(row_dict)
 
-    results = session.query(Parks.park_code, Parks.park_name, Parks.state, Parks.acres).all()
-
-    session.close()
-
-    allresults = list(np.ravel(results))
-
-    return jsonify(allresults)
+    return jsonify(results)
 
 
 
 @app.route("/api/v1.0/Visitation")
 def visitation():
-    session = Session(engine)
+    cursor = connection.cursor(cursor_factory = psycopg2.extras.DictCursor)
+    cursor.execute("SELECT * FROM parks_visitation")
+    columns = list(cursor.description)
+    result = cursor.fetchall()
+    results = []
+    for row in result:
+        row_dict = {}
+        for i, col in enumerate(columns):
+            row_dict[col.name] = row[i]
+        results.append(row_dict)
 
-    results = session.query(Visitation.park_name, Visitation.state, Visitation.visitors, Visitation.year).all()
-
-    session.close()
-
-    allresults = list(np.ravel(results))
-
-    return jsonify(allresults)
+    return jsonify(results)
 
 @app.route("/api/v1.0/Species")
 def species():
-    session = Session(engine)
+    cursor = connection.cursor(cursor_factory = psycopg2.extras.DictCursor)
+    cursor.execute("SELECT * FROM species")
+    columns = list(cursor.description)
+    result = cursor.fetchall()
+    results = []
+    for row in result:
+        row_dict = {}
+        for i, col in enumerate(columns):
+            row_dict[col.name] = row[i]
+        results.append(row_dict)
 
-    results = session.query(Species.park_name, Species.species_id, Species.category, Species.scientific_name, Species.common_names, Species.occurrence, Species.abundance).all()
-
-    session.close()
-
-    allresults = list(np.ravel(results))
-
-    return jsonify(allresults)
+    return jsonify(results)
 
 
 @app.route("/api/v1.0/Trails")
 def trails():
-    session = Session(engine)
+    cursor = connection.cursor(cursor_factory = psycopg2.extras.DictCursor)
+    cursor.execute("SELECT * FROM park_trails")
+    columns = list(cursor.description)
+    result = cursor.fetchall()
+    results = []
+    for row in result:
+        row_dict = {}
+        for i, col in enumerate(columns):
+            row_dict[col.name] = row[i]
+        results.append(row_dict)
 
-    results = session.query(Trails.trail_id, Trails.trail_name, Trails.park_name, Trails.city, Trails.state, Trails.popularity, Trails.length, Trails.elevation_gain, Trails.difficulty_rating, Trails.route_type, Trails.visitor_usage, Trails.units).all()
-
-    session.close()
-
-    allresults = list(np.ravel(results))
-
-    return jsonify(allresults)
+    return jsonify(results)
 
 
 @app.route("/api/v1.0/Trail_Features")
 def trail_feats():
-    session = Session(engine)
+    cursor = connection.cursor(cursor_factory = psycopg2.extras.DictCursor)
+    cursor.execute("SELECT * FROM trail_features")
+    columns = list(cursor.description)
+    result = cursor.fetchall()
+    results = []
+    for row in result:
+        row_dict = {}
+        for i, col in enumerate(columns):
+            row_dict[col.name] = row[i]
+        results.append(row_dict)
 
-    results = session.query(Trailfeats.park_name, Trailfeats.features, Trailfeats.id, Trailfeats.trail_id).all()
-
-    session.close()
-
-    allresults = list(np.ravel(results))
-
-    return jsonify(allresults)
+    return jsonify(results)
 
 
 @app.route("/api/v1.0/Trail_Activities")
 def trail_acts():
-    session = Session(engine)
+    cursor = connection.cursor(cursor_factory = psycopg2.extras.DictCursor)
+    cursor.execute("SELECT * FROM trail_activities")
+    columns = list(cursor.description)
+    result = cursor.fetchall()
+    results = []
+    for row in result:
+        row_dict = {}
+        for i, col in enumerate(columns):
+            row_dict[col.name] = row[i]
+        results.append(row_dict)
 
-    results = session.query(Trailacts.trail_id, Trailacts.activities, Trailacts.park_name, Trailacts.id).all()
-
-    session.close()
-
-    allresults = list(np.ravel(results))
-
-    return jsonify(allresults)
+    return jsonify(results)
       
 
 
