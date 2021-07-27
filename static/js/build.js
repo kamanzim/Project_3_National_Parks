493b9d23c92b69989b1638df8db03c203ddc2ffa
Derek V <derek.volz@gmail.com>
2021-07-27 11:05:25 -0500

    updated index.html and fixed the Park search in build.js. Updating the style.css with background color

--- static/js/build.js
+++ static/js/build.js
@@ -1,64 +1,70 @@
 // Get metadata from the samples.json file
 function metadata(s) {
   
-  d3.json("/api/v1.0/Species").then((data) => {
+  d3.json("/api/v1.0/Parks").then((data) => {
     
-    var setArray = data.filter(sObject => sObject.park_name == s);
-    // var result = setArray[0]
+    var parkName = data.filter(p => p.park_name == s);
+    console.log(parkName);
+    //var setArray = parkName.filter(sObject => sObject.park_name == s);
+    var result = parkName[0];
 
-    console.log(setArray);
+    console.log(result);
 
-    // var demographic = d3.select("#park-metadataa");
-    // demographic.html("");
-    // Object.entries(result).forEach(([key, value]) => {
-    //   demographic.append("h6").text(`${key}: ${value}`);
-    // });
+    
+
+    var demographic = d3.select("#park-metadata");
+    demographic.html("");
+    // demographic.append("h6").text(parkName);
+    Object.entries(result).forEach(([key, value]) => {
+      demographic.append("h6").text(`${key}: ${value}`);
+    });
   });
 }
 
-function addParks() {
+// function addParks() {
   
-  d3.select("#parkDataset").append('select')
-  d3.select("#parkDataset>select").append('option').text("Select a Park")
-  //d3.select("#parkDataset>select").attr('onchange','goToPark(this.value)')
-  d3.json("/api/v1.0/Parks").then(result=>{
-    var pNames = result.map(p => p.park_name);
-    result.forEach(park=>{
-      d3.select("#parkDataset>select").append('option').text(park.park_name).property('value',park.park_code)
-      });
-  //});
-    const initialPark = pNames[0];
-    console.log(pNames[0]);
-    //metadata(initialPark);
-  });
-}
+//   d3.select("#parkDataset").append('select')
+//   d3.select("#parkDataset>select").append('option').text("Select a Park")
+//   // d3.select("#parkDataset>select").attr('onchange','parkChange(this.value)')
+//   d3.json("/api/v1.0/Parks").then(result=>{
+//     var pNames = result.map(p => p.park_name);
+//     result.forEach(park=>{
+//       d3.select("#parkDataset>select").append('option').text(park.park_name).property('value',park.park_code)
+//       });
+//   //});
+//     const initialPark = pNames[0];
+//     //console.log(initialPark);
+//     metadata(initialPark);
+//   });
+// }
 
 function init() {
   // dropdown select element
-  // var selector = d3.select("#parkDataset");
-  addParks();
-  // fill in the selDataset
-  // d3.json("/api/v1.0/Parks").then(function(data) {
-  //   console.log(data[0].park_name)
-  // d3.json("/api/v1.0/Parks").then((p) => {
-  //   var pNames = p.park_name;
-  //   console.log(pNames)
-    // pNames.forEach((s) => {
-    //   selector
-    //     .append("option")
-    //     .text(s)
-    //     .property("value", s);
-    // });
+  var selector = d3.select("#parkDataset");
+  // addParks();
+  // fill in the parkDataset
+  //console.log(data.park_name);
+  d3.json("/api/v1.0/Parks").then((p) => {
+    var pNames = p.map(p => p.park_name);
+    console.log(pNames)
+    pNames.forEach((s) => {
+      selector
+      .append("option")
+      .text(s)
+      .property("value", s);
+    });
 
     // Pass in the first sample to create the graph and demographic info
-    // const initalSample = pNames[0];
-    // metadata(initalSample);
+    const initialPark = pNames[0];
+    console.log(initialPark)
+    metadata(initialPark);
     // buildGraphs(initalSample);
-  // });
+ });
 }
 
 function parkChange(nSample) {
   // Pass in the new sample once selected in the dropdown
+  //console.log(nSample)
   metadata(nSample);
   //buildGraphs(nSample);
 }
